e20e2b99e7e6a445a9a0169a88092739a65df931
aloulou1238 <benhibaali@gmail.com>
2023-05-31 12:18:50 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,92 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react'
+import Filter from './components/Filter'
+import PersonsForm from './components/PersonsForm'
+import Persons from './components/Persons'
+import Notification from './components/Notification'
+import PersonsService from './services/persons'
 
-function App() {
+const App = () => {
+
+  
+
+  const [persons, setPersons] = useState([]) 
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [filterText, setFilterText] = useState('')
+  const [notificationMessage, setNotificationMessage] = useState(null)
+  const [notificationType, setNotificationType] = useState('')
+
+  useEffect(() => {
+    PersonsService
+    .getAll()
+    .then(initialPersons => {
+      setPersons(initialPersons)
+    })
+  }, [persons])
+
+  let filtered = persons.filter(person => person.name && person.name.toLowerCase().includes(filterText.toLowerCase()))
+
+  const addPerson = (e) => {
+    e.preventDefault()
+    if(!newName && !newNumber){
+      alert('Please fill the form')
+      return
+    }
+    const foundName = persons.find((person) => person.name === newName)
+    const foundNumber = persons.find((person) => person.number === newNumber)
+    if (foundName && foundNumber) {
+      alert(`${newName} already exists`)
+    } else if (foundName && !foundNumber) {
+      if (window.confirm(`${newName} is already added to phonebook, replace the old number with a new one?`))  { 
+        const changedPerson = {...foundName, number: newNumber}
+        PersonsService
+          .updateNumber(foundName.id, changedPerson) 
+          .then(returnedPerson => {
+            setPersons(persons.map(person => foundName !== newName ? person : returnedPerson))
+          })
+          .catch(error => {
+            setNotificationType('error')
+            setNotificationMessage(`Information of ${newName} has already been removed from server`)
+            setTimeout(() => {
+              setNotificationMessage(null)
+            }, 5000);
+          })
+      } 
+      setNotificationType('success')
+      setNotificationMessage(`${newName}`)
+      setTimeout(() => {
+        setNotificationMessage(null)
+      }, 5000);
+    } else {
+      PersonsService
+        .addPerson({name: newName, number: newNumber})
+        .then(newPerson => setPersons(persons.concat(newPerson)))
+      setNotificationType('success')
+      setNotificationMessage(`${newName}`)
+      setTimeout(() => {
+        setNotificationMessage(null)
+      }, 5000);
+    }
+
+    setNewName('')
+    setNewNumber('')
+  }
+
+  const filterNumbers = (e) => {
+    setFilterText(e.target.value)
+  }  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Phonebook</h2>
+      <Notification type={notificationType} message={notificationMessage}/>
+      <Filter filterNumbers={filterNumbers} filterText={filterText}/>
+      <h2>Add a new</h2>
+      <PersonsForm newName={newName} newNumber={newNumber} addPerson={addPerson} setNewName={setNewName} setNewNumber={setNewNumber}/>
+      <h2>Numbers</h2>
+      <Persons persons={persons} filterText={filterText} filtered={filtered}/>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
